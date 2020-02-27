18037e50a0727fa202bd21b7095c72aa8ffc6cb4
Niko Granö <niko@ironlions.fi>
2020-02-27 18:13:26 +0200

    Initialize locale support

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,18 @@
 module.exports = {
     lintOnSave: false,
-
     pluginOptions: {
-      express: {
-        shouldServeApp: true,
-        serverDir: './srv'
-      },
-      autoRouting: {
-        chunkNamePrefix: 'page-'
-      }
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            enableInSFC: false
+        },
+        express: {
+            shouldServeApp: true,
+            serverDir: './srv'
+        },
+        autoRouting: {
+            chunkNamePrefix: 'page-'
+        }
     },
     transpileDependencies: ['vuex-module-decorators']
 };
