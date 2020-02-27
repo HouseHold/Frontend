84dbf81999879e8e05ec0b678ae729fdc9ea2261
Niko Granö <niko@ironlions.fi>
2020-02-27 18:13:26 +0200

    Initialize locale support

---
+++ src/locales/index.ts
@@ -0,0 +1,8 @@
+import en from  './en.json';
+//import fi from './fi.json';
+
+export const defaultLocale = 'en';
+export const messages = {
+    en: en,
+    //fi: fi,
+};
--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,23 @@ import ApolloClient from 'apollo-boost';
 import VCalendar from 'v-calendar';
 import { iconsSet as icons } from './assets/icons/icons';
 import store from './store';
+import VueI18n from "vue-i18n";
+import { defaultLocale, messages } from './locales';
 
 Vue.config.performance = true;
 Vue.use(CoreuiVue);
 Vue.use(VueApollo);
 Vue.use(VCalendar, { componentPrefix: '' });
+Vue.use(VueI18n);
 
 const apolloClient = new ApolloClient({ uri: window.location.protocol + '//' + window.location.host + '/' + 'api/graphql' });
 const apolloProvider = new VueApollo({ defaultClient: apolloClient });
+const i18n = new VueI18n({ locale: defaultLocale, fallbackLocale: defaultLocale, messages });
 
 new Vue({
     el: '#app',
     apolloProvider,
+    i18n,
     router,
     // @ts-ignore
     icons,
@@ -28,3 +33,12 @@ new Vue({
     template: '<App/>',
     store
 });
+
+
+// Development related
+if (module.hot) {
+    module.hot.accept(['./locales/en.json'], function () {
+        i18n.setLocaleMessage('en', require('./locales/en.json'));
+        //i18n.setLocaleMessage('fi', require('./translations/fi.json'));
+    });
+}
